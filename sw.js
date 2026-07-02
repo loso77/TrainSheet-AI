c8e9af06591fb51fe8d65b7e8eec4f00e96a5dc3
loso77 <58019098+loso77@users.noreply.github.com>
2026-07-02 11:47:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-const CACHE_NAME="trainsheet-ai-v0-4-0";const FILES_TO_CACHE=["./","./index.html","./style.css","./app.js","./manifest.json"];self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)))});self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
+/* TrainSheet AI Version: 0.5.0 */
+const CACHE_NAME="trainsheet-ai-v0-5-0";
+const FILES_TO_CACHE=["./","./index.html","./style.css","./app.js","./manifest.json"];
+self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)))});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
+self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
