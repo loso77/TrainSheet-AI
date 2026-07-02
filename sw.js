ecad0e2383e879eae4c0ad0f3384c6707ca205e6
loso77 <58019098+loso77@users.noreply.github.com>
2026-07-02 12:03:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* TrainSheet AI Version: 0.5.0 */
-const CACHE_NAME="trainsheet-ai-v0-5-0";
+/* TrainSheet AI Version: 0.6.0 */
+const CACHE_NAME="trainsheet-ai-v0-6-0";
 const FILES_TO_CACHE=["./","./index.html","./style.css","./app.js","./manifest.json"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
