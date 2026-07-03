fe217b92b116d0170bdc698243c0d7b430949ed4
loso77 <58019098+loso77@users.noreply.github.com>
2026-07-04 04:49:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "trainsheet-v2.4.0";
+const CACHE_NAME = "trainsheet-v2.4.1";
 const APP_FILES = ["./","./index.html","./style.css","./app.js","./manifest.json"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
